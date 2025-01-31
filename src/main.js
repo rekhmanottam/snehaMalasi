28680be5db7994d6e2563f20c9fd7d67ec0de4e5
Utkarsh Malasi <utkarshmalasi2910@gmail.com>
2025-01-31 22:40:43 +0530

    no

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
 import React from "react";
-createRoot(document.getElementById("root")).render(<App />);
+import { createRoot } from "react-dom/client";
+import App from "./src/App.js";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+createRoot(rootElement).render(<App />);
